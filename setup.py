b2e5bb7643c85ca11545bb48c20241b00b817a87
Jonathan Balls <j.balls@btopenworld.com>
2015-01-07 15:59:17 +0000

    added setup.py for pip and easy_install installation

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='Cabot Hipchat Plugin',
+      version='1.0',
+      description='A hipchat alert plugin for Cabot by Arachnys',
+      author='Arachnys',
+      author_email='info@arachnys.org',
+      url='http://cabotapp.com',
+      packages=[
+      	],
+     )
