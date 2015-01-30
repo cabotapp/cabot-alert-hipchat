9d8853110acef27dfcdc741ce200113c2b34b87c
Jonathan Balls <j.balls@btopenworld.com>
2015-01-30 13:00:14 +0000

    updated setup file

--- setup.py
+++ setup.py
@@ -1,15 +1,13 @@
 #!/usr/bin/env python
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='cabot-alert-hipchat',
-      version='1.0',
+      version='1.2',
       description='A hipchat alert plugin for Cabot by Arachnys',
       author='Arachnys',
       author_email='info@arachnys.org',
       url='http://cabotapp.com',
-      packages=[
-      	'cabot_alert_hipchat'
-      	],
-      download_url= 'https://github.com/bonniejools/cabot-alert-hipchat/tarball/0.1'
+      packages=find_packages(),
+      download_url = 'https://github.com/bonniejools/cabot-alert-hipchat/tarball/1.2',
      )
