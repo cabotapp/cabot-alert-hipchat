2511fdd5c9953587bbfc4912107e163f83a819d7
Frank Hamand <frankhamand@gmail.com>
2017-02-01 11:18:41 +0000

    Update to 1.8.3
    I was naughty and pushed to pypi to develop. I didn't realise some
    breaking changes had been pushed since the last tag. Skip to good tag

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@
 from setuptools import setup, find_packages
 
 setup(name='cabot-alert-hipchat',
-      version='1.8.0',
+      version='1.8.3',
       description='A Hipchat alert plugin for Cabot by Arachnys',
       author='Arachnys',
       author_email='info@arachnys.com',
       url='http://cabotapp.com',
       packages=find_packages(),
-      download_url='https://github.com/cabotapp/cabot-alert-hipchat/archive/1.8.0.zip',
+      download_url='https://github.com/cabotapp/cabot-alert-hipchat/archive/1.8.3.zip',
      )
