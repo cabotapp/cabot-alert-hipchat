e1d73693b867bee33737717a03d6d666a5de22c2
Frank Hamand <frankhamand@gmail.com>
2017-04-03 20:16:14 +0100

    2.0.1

--- setup.py
+++ setup.py
@@ -2,12 +2,14 @@
 
 from setuptools import setup, find_packages
 
+VERSION = '2.0.1'
+
 setup(name='cabot-alert-hipchat',
-      version='2.0.0',
+      version=VERSION,
       description='A Hipchat alert plugin for Cabot by Arachnys',
       author='Arachnys',
       author_email='info@arachnys.com',
       url='http://cabotapp.com',
       packages=find_packages(),
-      download_url='https://github.com/cabotapp/cabot-alert-hipchat/archive/1.8.3.zip',
+      download_url='https://github.com/cabotapp/cabot-alert-hipchat/archive/{}.zip'.format(VERSION),
      )
